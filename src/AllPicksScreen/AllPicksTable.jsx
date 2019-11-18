f3a82ae2b3a8428c5970b4506abb1909c6be810e
Justin Carruthers <jtcruthers@gmail.com>
2019-11-17 21:53:09 -0500

    Showing logos for picks

--- src/AllPicksScreen/AllPicksTable.jsx
+++ src/AllPicksScreen/AllPicksTable.jsx
@@ -1,10 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import {getImageUrlFromUri} from '../utils';
 
+const Pick = ({bowl, pick}) => {
+    const pickName = pick === 'home' ? bowl.homeTeam.name : bowl.awayTeam.name; 
+    const uri = pick === 'home'? bowl.homeTeam.uri : bowl.awayTeam.uri;
+    const imageUrl = getImageUrlFromUri(uri);
+    return (
+        <td>
+            <img src={imageUrl} alt={`${pickName} logo`} width={50} height={50} />
+            <p>{pick === 'home' ? bowl.homeTeam.name : bowl.awayTeam.name}</p>
+        </td>
+    )
+}
+
+const StyledTable = styled.table`
+    border-collapse: separate;
+    border-spacing: 0 2em;
+`;
 
 const AllPicksTable = ({bowls, picks, picksPerBowl}) => (
-    <table>
+    <StyledTable>
         <thead>
             <tr>
                 <th>Matchup</th>
@@ -16,12 +33,12 @@ const AllPicksTable = ({bowls, picks, picksPerBowl}) => (
                 <tr key={bowl.id}>
                     <td>{`${bowl.awayTeam.name} vs ${bowl.homeTeam.name}`}</td>
                     {
-                        picksPerBowl[bowl.id].map((pick, index) => <td key={index}>{pick}</td>)
+                        picksPerBowl[bowl.id].map((pick, index) => <Pick bowl={bowl} pick={pick} key={index} />)
                     }
                 </tr>
             ))}
         </tbody>
-    </table>
+    </StyledTable>
 )
 
 export default AllPicksTable;
